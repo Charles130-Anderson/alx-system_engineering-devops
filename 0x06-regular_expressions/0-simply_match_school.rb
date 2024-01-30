468f485b726649a99a6ff82d5600117cb14574a4
Charles Anderson <andersonoyoo034@gmail.com>
2024-01-30 23:34:27 +0300

    An array method that retrirves and seaches for the occurances of school

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,10 +1,2 @@
 #!/usr/bin/env ruby
-# The above shebang (#!) line specifies the path to the Ruby interpreter,
-# allowing the script to be executed directly from the command line.
-
 puts ARGV[0].scan(/School/).join
-# "puts" is a Ruby method for printing to the console.
-# ARGV is an array that holds the command line arguments passed to the script.
-# ARGV[0] retrieves the first command line argument.
-# .scan(/school/) searches for occurrences of the regex pattern "school" in the input string.
-# .join concatenates the matching portions into a single string and prints it.
