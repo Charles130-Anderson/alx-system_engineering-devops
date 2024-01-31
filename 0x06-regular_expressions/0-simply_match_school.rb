c8f64ac95c7adccb9e4a399714981f7c5d9a48b7
Charles Anderson <andersonoyoo034@gmail.com>
2024-01-31 14:57:34 +0300

    r0x06-regular expressions

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/env ruby
 # The above shebang (#!) line specifies the path to the Ruby interpreter,
 # allowing the script to be executed directly from the command line.
-
 # ARGV is an array that holds the command line arguments passed to the script.
 # ARGV[0] retrieves the first command line argument.
 # .scan(/School/) searches for occurrences of the regex pattern "School" in the input string.
