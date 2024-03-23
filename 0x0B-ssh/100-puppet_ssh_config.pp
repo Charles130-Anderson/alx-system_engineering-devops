a0ebd2feca5db6c50620f35daf8c5a4daaf293f8
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-03-23 11:48:53 -0700

    0x0B-SSH

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,19 @@
-#!/usr/bin/env bash
-# using puppet to make changes to the config file
-
-file_line { 'etc/ssh/ssh_config':
-             ensure => present,
-content =>"
-        host* IdentifyFile ~/.ssh/school
-        passwordAuthentication no
-	",
+# Define a Puppet class to manage SSH configuration
+class ssh_config {
+  # Ensure the SSH configuration file exists
+  file { '/etc/ssh/ssh_config':
+    ensure  => present,
+    # Set the content of the file
+    content => "
+      # SSH client configuration to use private key and disable password authentication
+      Host *
+        IdentityFile ~/.ssh/school
+        PasswordAuthentication no
+    ",
+    # Notify the SSH service to reload if changes are made
+    notify  => Service['ssh'],
+  }
 }
+
+# Apply the SSH configuration class
+include ssh_config
