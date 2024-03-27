a3bfe9cc3b65b2d7c3f724df021c28d23ea26710
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-03-27 03:39:08 -0700

    Web Server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,17 +1,38 @@
-# Puppet automation
+# Puppet manifest to install and configure Nginx web server
+
+# Install Nginx package
 package { 'nginx':
   ensure => installed,
 }
-file { 'install':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-enabled/default',
-  after  => 'listen 80 default_server',
-  line   => '^\/redirect_me https:\/\/github.com\/Charles130-Anderson permanent;',
-}
-file { '/var/www/html/index.html':
-  content => 'Hello world!'
-}
-service { 'nginx':
-  ensure  => running,
+
+# Configure Nginx server
+file { '/etc/nginx/sites-enabled/default':
+  ensure  => present,
+  content => "
+            server {
+              listen 80 default_server;
+              root /var/www/html;
+              index index.html index.htm;
+              location / {
+                try_files \$uri \$uri/ =404;
+              }
+              location = /redirect_me {
+                return 301 https://github.com/Charles130-Anderson;
+              }
+            }
+            ",
   require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+# Ensure the content of /var/www/html/index.html contains "Hello World!"
+file { '/var/www/html/index.html':
+  ensure  => present,
+  content => 'Hello World!',
+}
+
+# Ensure Nginx service is running
+service { 'nginx':
+  ensure => running,
+  enable => true,
 }
