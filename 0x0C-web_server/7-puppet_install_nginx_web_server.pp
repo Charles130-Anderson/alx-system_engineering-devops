20207b77f5c2e2bb2d3d69419c051cf618be34ae
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-03-27 03:29:46 -0700

    Web Server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,17 @@
+# Puppet autiomation
+package { 'nginx':
+  ensure  => installed,
+}
+file {'install':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'listen 80 default_server',
+  line   => '^\/redirect_me https:\/\/github.com\/Charles130-Anderson permanent;',
+}
+file { '/var/www/html/index.html':
+  content => 'Hello world!'
+}
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
