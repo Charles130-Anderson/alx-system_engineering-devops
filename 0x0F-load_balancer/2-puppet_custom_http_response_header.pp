6b17b1b28df7e8b2584d09d4b306fe674129ff66
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-04-01 00:16:48 -0700

    Load Balancer

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,22 +1,38 @@
-# File: 2-puppet_custom_http_response_header.pp
-
 # Define a class to manage the custom HTTP header
 class custom_http_response_header {
 
-  # Ensure the 'nginx' package is installed
+  # Update package information
+  exec { 'apt-update':
+    command => '/usr/bin/apt-get -y update',
+    path    => ['/usr/bin', '/bin'],
+  }
+
+  # Install Nginx package
   package { 'nginx':
     ensure => installed,
+  }
+
+  # Create a basic HTML file
+  file { '/var/www/html/index.html':
+    content => 'Hello World!',
   }
 
   # Define a custom fact to retrieve the server's hostname
   $server_hostname = $facts['hostname']
 
-  # Define a file resource to add the custom HTTP header configuration
-  file { '/etc/nginx/conf.d/custom_headers.conf':
-    ensure  => present,
-    content => "add_header X-Served-By $server_hostname;\n",
+  # Configure Nginx to add the custom HTTP header
+  file_line { 'add custom header':
+    ensure => present,
+    path   => '/etc/nginx/sites-available/default',
+    line   => "\tadd_header X-Served-By ${server_hostname};",
+    after  => 'server_name _;',
     require => Package['nginx'],
     notify  => Service['nginx'],
+  }
+
+  # Ensure Nginx is running
+  service { 'nginx':
+    ensure => running,
   }
 
 }
