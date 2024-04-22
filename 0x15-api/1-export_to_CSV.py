518e4cae63b330683ab926e6b5bf9caf639cb598
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-04-22 04:45:37 -0700

    API

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,46 +1,21 @@
 #!/usr/bin/python3
-"""
-Returns to-do list information for a given employee ID and exports it to CSV.
-"""
+"""Exports to-do list information for a given employee ID to CSV format."""
 
 import csv
 import requests
 import sys
 
-
-def export_to_csv(employee_id, user_name, todo_data):
-    file_name = "{}.csv".format(employee_id)
-    with open(file_name, mode='w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(['USER_ID', 'USERNAME',
-                         'TASK_COMPLETED_STATUS', 'TASK_TITLE'])
-        for task in todo_data:
-            writer.writerow([
-                employee_id,
-                user_name, str(task['completed']),
-                task['title']])
-
-
 if __name__ == "__main__":
+    user_id = sys.argv[1]
     base_url = "https://jsonplaceholder.typicode.com/"
-    employee_id = sys.argv[1]
-
-    user_response = requests.get(base_url + "users/{}".format(employee_id))
-    todo_response = requests.get(
+    user_data = requests.get(base_url + "users/{}".format(user_id)).json()
+    username = user_data.get("username")
+    todo_data = requests.get(
         base_url + "todos",
-        params={"userId": employee_id}
-        )
-
-    if user_response.status_code != 200:
-        print("User data retrieval failed!")
-        sys.exit(1)
-
-    if todo_response.status_code != 200:
-        print("TODO data retrieval failed!")
-        sys.exit(1)
-
-    user_data = user_response.json()
-    todo_data = todo_response.json()
-
-    export_to_csv(employee_id, user_data['name'], todo_data)
-    print("Data exported to {}.csv".format(employee_id))
+        params={"userId": user_id}
+    ).json()
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, task.get("completed"), task.get("title")]
+         ) for task in todo_data]
