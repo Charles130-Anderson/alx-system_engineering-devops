ec88e0d0cff38c53afa8c04715ca0783bf7ceafe
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-04-22 04:13:57 -0700

    API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,35 +1,19 @@
 #!/usr/bin/python3
 """
-Using a REST API, for a given employee ID, returns information about his/her TODO list progress.
+Returns to-do list information for a given employee ID.
 """
 
-import sys
 import requests
+import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
-        print("Usage: {} employee_id".format(sys.argv[0]))
-        sys.exit(1)
-
-    employee_id = int(sys.argv[1])
-
-    url_user = 'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)
-    url_todos = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(employee_id)
-
-    try:
-        response_user = requests.get(url_user)
-        response_todos = requests.get(url_todos)
-        user_data = response_user.json()
-        todos_data = response_todos.json()
-    except requests.exceptions.RequestException as e:
-        print("Error:", e)
-        sys.exit(1)
-
-    employee_name = user_data.get('name')
-    total_tasks = len(todos_data)
-    completed_tasks = [task for task in todos_data if task.get('completed')]
-    num_completed_tasks = len(completed_tasks)
-
-    print("Employee {} is done with tasks({}/{}):".format(employee_name, num_completed_tasks, total_tasks))
-    for task in completed_tasks:
-        print("\t{}".format(task.get('title')))
+    base_url = "https://jsonplaceholder.typicode.com/"
+    employee_id = sys.argv[1]
+    user_data = requests.get(base_url + "users/{}".format(employee_id)).json()
+    todo_params = {"userId": employee_id}
+    todo_data = requests.get(base_url + "todos", todo_params).json()
+    completed_tasks = [task.get("title") for task in todo_data
+                       if task.get("completed")]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user_data.get("name"), len(completed_tasks), len(todo_data)))
+    [print("\t {}".format(task)) for task in completed_tasks]
