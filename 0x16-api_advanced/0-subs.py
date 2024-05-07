c9451e8ef89c555a92ed3995bbe22cf96787f698
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-05-07 10:31:47 -0700

    querying for sub reddit subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,13 +12,11 @@ def number_of_subscribers(subreddit):
     If not a valid subreddit, returns 0.
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "MyAPI/0.1"}
+    headers = {"User-Agent": "MyAPI/1.0.0 (by /u/Anderson"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-
     if response.status_code == 200:
         data = response.json()
         subscribers = data['data']['subscribers']
         return subscribers
     else:
         return 0
-
