0164edd3476e7ce8d1ac5d63106f3bba6f7c5a53
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-05-08 14:25:30 -0700

    datadog setup

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,6 @@
-{ 'fix-apache-error':
+# trace_is_your_friend.pp fixing typo
+
+exec { 'fix-apache-error':
         command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
         provider => 'shell'
 }
