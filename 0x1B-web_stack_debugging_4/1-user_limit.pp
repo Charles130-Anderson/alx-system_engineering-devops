ad6d0d5145d2bc0743b4142fb2241981f2c5b2f1
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-05-16 09:30:24 -0700

    configuring login

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,12 @@
-# Fix problem of high amount files opened
-
-exec {'replace-1':
+# Increase the limit of open files for the user
+exec {'increase-file-limit-1':
   provider => shell,
   command  => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
-  before   => Exec['replace-2'],
+  before   => Exec['increase-file-limit-2'],
 }
 
-exec {'replace-2':
+# Further increase the limit of open files for the user
+exec {'increase-file-limit-2':
   provider => shell,
   command  => 'sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
 }
